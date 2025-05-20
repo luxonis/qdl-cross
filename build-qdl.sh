39d56e61f870cfb56f2ea0f8d3cb2ff5dae68d09
Filip Jeretina <filip.jeretina@luxonis.com>
2025-05-20 13:29:01 +0200

    Create tar.gz on macos.

--- build-qdl.sh
+++ build-qdl.sh
@@ -4,8 +4,9 @@ set -e
 git submodule update --init --recursive
 
 SCRIPT_DIR=$(dirname $(realpath "$0"))
-OUT_DIR=${SCRIPT_DIR}/.out/qdl/
-mkdir -p $OUT_DIR
+OUT_DIR_BASE=${SCRIPT_DIR}/.out
+OUT_DIR_QDL=${OUT_DIR_BASE}/qdl
+mkdir -p $OUT_DIR_QDL
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     is_macos=true
@@ -40,15 +41,15 @@ function build_qdl_linux {
     # Libusb (LGPL) can be statically linked for simplicity since https://github.com/luxonis/qdl-cross is provided.
     # https://www.gnu.org/licenses/gpl-faq.html#GPLIncompatibleLibs
     make -j8 CFLAGS='-I ../libxml2/include/ -I ../libusb/libusb/' LDFLAGS='-static ../libusb/libusb/.libs/libusb-1.0.a ../libxml2/.libs/libxml2.a -lm -lc'
-    cp ./qdl ${OUT_DIR}
+    cp ./qdl ${OUT_DIR_QDL}
     popd
 }
 
 function build_qdl_macos {
     pushd qdl
     make -j8 CFLAGS='-I ../libxml2/include/ -I ../libusb/libusb/' LDFLAGS='-L ../libusb/libusb/.libs/ -lusb-1.0 -L ../libxml2/.libs/ -lxml2 -lm -lc'
-    dst_lib_dir=${OUT_DIR}/lib/
-    dst_bin_dir=${OUT_DIR}/bin/
+    dst_lib_dir=${OUT_DIR_QDL}/lib/
+    dst_bin_dir=${OUT_DIR_QDL}/bin/
     mkdir -p ${dst_bin_dir}
     mkdir -p ${dst_lib_dir}
     cp ../libxml2/.libs/libxml2.16.dylib ${dst_lib_dir}
@@ -56,6 +57,7 @@ function build_qdl_macos {
     cp ./qdl ${dst_bin_dir}
     # Note that to make the QDL work, you need to control the location of the dylibs (or add them to a standard search dir). You can modify the rpath in the binary.
     # for example: install_name_tool -add_rpath "/usr/local/lib/oakctl/" ./qdl
+    tar czf ${OUT_DIR_BASE}/qdl.tar ${dst_bin_dir}/* ${dst_lib_dir}/*
 }
 
 build_libxml
