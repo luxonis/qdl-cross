8b339ddd3dd668cd694ed0ef0edf862d0e8e99e9
zrezke <jeretina.filip@gmail.com>
2025-05-21 19:32:31 +0200

    go into the qdl dir when creating tar gz.

--- build-qdl.sh
+++ build-qdl.sh
@@ -66,7 +66,9 @@ function build_qdl_macos {
     cp ../libxml2/.libs/libxml2.16.dylib "${dst_lib_dir}"
     cp ../libusb/libusb/.libs/libusb-1.0.0.dylib "${dst_lib_dir}"
     cp ./qdl "${dst_bin_dir}"
-    tar czf "${OUT_DIR_BASE}/qdl-${PLATFORM}.tar.gz" "${dst_bin_dir}"/* "${dst_lib_dir}"/*
+    pushd "${OUT_DIR_QDL}"
+    tar czf "${OUT_DIR_BASE}/qdl-${PLATFORM}.tar.gz" "./*"
+    popd
     popd
 }
 
@@ -79,7 +81,9 @@ function build_qdl_windows {
     cp ../libxml2/.libs/libxml2-16.dll "${dst_lib_dir}"
     cp ../libusb/libusb/.libs/libusb-1.0.dll "${dst_lib_dir}"
     cp ./qdl "${dst_bin_dir}"
-    tar czf "${OUT_DIR_BASE}/qdl-${PLATFORM}.tar.gz" "${dst_bin_dir}"/* "${dst_lib_dir}"/*
+    pushd "${OUT_DIR_QDL}"
+    tar czf "${OUT_DIR_BASE}/qdl-${PLATFORM}.tar.gz" "./*"
+    popd
     popd
 }
 
